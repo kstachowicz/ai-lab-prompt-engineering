8a8823457e9245677753f0097dd4f6c683cb5c61
kastacho <kastacho@microsoft.com>
2023-07-16 00:13:07 +0200

    filled up python and dotnet notebooks

---
+++ dotnet/AzureOpenAIConfig.cs
@@ -0,0 +1,6 @@
+public class AzureOpenAIConfig
+{
+    public string AZURE_OPENAI_KEY { get; set; }
+    public string AZURE_OPENAI_ENDPOINT { get; set; }
+    public string AZURE_OPENAI_DEPLOYMENT_NAME { get; set; }
+}
